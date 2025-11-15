17c6dc9af342ce8dad40ebfceb093fc35ca10f92
Daniel Rodríguez Tarry <drtarry@uw.edu>
2025-11-14 18:58:30 -0800

    Cleaning

--- deg2km.py
+++ deg2km.py
@@ -1,33 +1,33 @@
-'''
-autor: @Tarry
+"""
+Convert geographic coordinates to Cartesian distances
 
-type: FUNCTION
+Inputs:
+    lon1 (float): Longitude of first point in degrees
+    lat1 (float): Latitude of first point in degrees
+    lon2 (float): Longitude of second point in degrees
+    lat2 (float): Latitude of second point in degrees
 
-Moving average filter
-
-IN:
-	- lon1:		Longitude of first point
-	- lat1:		Longitude of first point
-	- lon2:		Longitude of second point
-	- lat2:		Longitude of second point
-
-OUT:
-	- dx:		Distance in km Eastwards
-	- dy:		Distance in km Northwards
-'''
-
+Returns:
+    tuple: (dx, dy) where:
+        - dx (float): Distance in km Eastwards
+        - dy (float): Distance in km Northwards
+"""
 import numpy as np
 
-def deg2km(lon1,lat1,lon2,lat2):
-	# Distance in degrees calculation
-	dlon = lon2 - lon1
-	dlat = lat2 - lat1
-
-	# Latitude mean calculation
-	latm = (lat1+lat2)/2.
-
-	# Cartesian distance calculation
-	dx = np.pi/180.*np.cos(np.radians(latm))*6371000*dlon
-	dy = np.pi/180.*6371000*dlat
-
-	return (dx/1000.,dy/1000.)
+def deg2km(lon1, lat1, lon2, lat2):
+    # Calculate distance in degrees
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    
+    # Calculate mean latitude for projection
+    latm = (lat1 + lat2) / 2.0
+    
+    # Convert to Cartesian distances (km)
+    # Earth radius: 6371 km
+    EARTH_RADIUS_M = 6371000
+    DEG_TO_RAD = np.pi / 180.0
+    
+    dx = DEG_TO_RAD * np.cos(np.radians(latm)) * EARTH_RADIUS_M * dlon
+    dy = DEG_TO_RAD * EARTH_RADIUS_M * dlat
+    
+    return (dx / 1000.0, dy / 1000.0)
