f18547b723eddef56b0636bdb1fbee6b96e4ddc4
Daniel Rodríguez Tarry <drtarry@uw.edu>
2025-11-14 19:05:44 -0800

    Reorganization and cleaning

--- matlab2python_time.py
+++ matlab2python_time.py
@@ -1,16 +1,19 @@
-"""
-@author: Tarry
-
-type: FUNCTION
-
-description: function to convert matlab time into number
-input: time data
-output: time in sec
-"""
-
 import pandas as pd
 
 def get_time(time):
-	t = pd.to_datetime(time,unit='D')
-	tsec = t.minute*60 + t.second + t.microsecond*1e-6
-	return(tsec)
+    """
+    Convert MATLAB time format to seconds within the current minute.
+    
+    Args:
+        time (float): MATLAB serial date number (days since January 0, 0000)
+    
+    Returns:
+        float: Time in seconds (minutes * 60 + seconds + microseconds)
+    """
+    # Convert MATLAB serial date to datetime
+    t = pd.to_datetime(time, unit='D')
+    
+    # Extract time components and convert to seconds
+    tsec = t.minute * 60 + t.second + t.microsecond * 1e-6
+    
+    return tsec
